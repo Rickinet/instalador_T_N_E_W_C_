3831c350d3c1ddf2b5e740f8ed7dadf2e22ee3a5
Rickinet <48504862+Rickinet@users.noreply.github.com>
2023-10-13 19:54:59 -0300

    Update 2evolution-api-proxy.sh

--- 2evolution-api-proxy.sh
+++ 2evolution-api-proxy.sh
@@ -350,7 +350,7 @@ echo "Sua ApiKey Global: $keyy"
 echo ""
 echo ""
 
-cd ~/instalador_T_N_E
+cd /home/ubuntu/instalador_T_N_E_W_C_
 
 # Retorna para o instalador.sh
 # Exibe uma mensagem de confirmação
